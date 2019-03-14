2cc60188ff8a0c54c69fbe529f05f30b86f01f2c
Benjides <benjergara@gmail.com>
2019-03-14 18:45:13 +0100

    Network builder framework

---
+++ module/network/network_builder.py
@@ -0,0 +1,38 @@
+"""Network builder"""
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+from module.network.bp_mll import bp_mll_loss
+
+LAYERS = {
+    'Dense':    lambda args: Dense(**args),
+    'Dropout':  lambda args: Dropout(**args),
+}
+
+LOSSES = {
+    "bp_mll_loss": bp_mll_loss
+}
+
+def builder(x_dim, y_dim, config):
+    """Network parser builder
+
+    Genarates a Keras ANN architecture using a config file
+
+    Parameters
+    ----------
+        config: dictionary containing the specified configuration
+
+    Returns
+    -------
+        model: Keras ANN
+
+    """
+    model = Sequential()
+    config['layers'][0]['args']['input_dim'] = x_dim
+    config['layers'][-1]['args']['units'] = y_dim
+    for layer in config['layers']:
+        l = LAYERS[layer['layer']](layer['args'])
+        model.add(l)
+    loss = config['compile']['loss']
+    config['compile']['loss'] = LOSSES.get(loss, loss)
+    model.compile(**config['compile'])
+    return model
